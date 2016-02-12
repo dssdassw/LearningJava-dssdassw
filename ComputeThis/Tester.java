4c6be42919b69cc3692966c2a049dc955724d6ca
unknown <674843024@ntdm-134-12.hcdsb.edu.on.ca>
2016-02-12 12:43:38 -0500

    Generic commit message hereÎÅPlÎÅs:';;:;PleaseÎÐÎÐÎÐÎÐÎÐÎÅÎÐÎÐeaseÎÐÎÐÎÐÎÐÎÐÎÅÎÐÎÐÎÅPleaseÎÐÎÐÎÐÎÐÎÐÎ Å :x!
    ::
    ;:::Xx;x;

---
+++ ComputeThis/Tester.java
@@ -0,0 +1,16 @@
+import java.io.PrintWriter; //even though the class it extends imports this, it won't compile otherwise
+
+public class Tester extends Tools {
+    public static double equation1() {
+        return (3*Math.PI*Math.sin(Math.toRadians(187))+Math.abs(Math.cos(Math.toRadians(122))));
+    }
+    public static double equation2() {
+        return (Math.pow(14.72, 3.801)+Math.log(72D));
+    }
+    public static void main(String[] args) {
+        //PrintWriter stdout = new PrintWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8), true);
+        PrintWriter stdout = getPW();
+        stdout.println("3πsin(187°) + |cos(122°)| = " + (equation1()));
+        stdout.println("(14.72)^3.801 + ln 72 = " + (equation2()));
+    }
+}
--- Tools/Tools.java
+++ Tools/Tools.java
@@ -1,6 +1,23 @@
+//for getPrintWriter() {
+import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+//}
 public class Tools {
     public static void prnt(String toPrint){
         System.out.println(toPrint);
     }
-    
+    public static int find(String[] searchString, String thing) {
+        int pos = -1;
+        for(int i = 0; i < searchString.length; i++) {
+          if(searchString[i] == thing) {
+                pos = i;
+                break;
+            }
+        }
+        return pos;
+    }
+    public static PrintWriter getPW() { //can't do optional arguments lol so just UTF-8, fuck you
+		return (new PrintWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8), true));
+	}
 }
