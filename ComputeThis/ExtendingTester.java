6cbfd41d2a4ccf46218cadaf8fd81c3b87f1dd87
unknown <674843024@ntdm-134-12.hcdsb.edu.on.ca>
2016-02-12 13:05:34 -0500

    This commit doesn't look like a piece of shit, I promise.

--- ComputeThis/ExtendingTester.java
+++ ComputeThis/ExtendingTester.java
@@ -1,8 +1,6 @@
 import java.io.PrintWriter; //even though the class it extends imports this, it won't compile otherwise
-import java.io.OutputStreamWriter;
-import java.nio.charset.StandardCharsets;
 
-public class Tester {
+public class Tester extends Tools {
     public static double equation1() {
         return (3*Math.PI*Math.sin(Math.toRadians(187))+Math.abs(Math.cos(Math.toRadians(122))));
     }
@@ -10,7 +8,8 @@ public class Tester {
         return (Math.pow(14.72, 3.801)+Math.log(72D));
     }
     public static void main(String[] args) {
-        PrintWriter stdout = new PrintWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8), true);
+        //PrintWriter stdout = new PrintWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8), true);
+        PrintWriter stdout = getPW();
         stdout.println("3πsin(187°) + |cos(122°)| = " + (equation1()));
         stdout.println("(14.72)^3.801 + ln 72 = " + (equation2()));
     }
